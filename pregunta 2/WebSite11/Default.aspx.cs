8f8d74742baca27bb4368be0cca17208459589d1
FabioVlad <81887721+FabioVlad@users.noreply.github.com>
2021-06-01 21:24:50 -0400

    Add files via upload

---
+++ pregunta 2/WebSite11/Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ServiceReference1.WebServiceSoapClient ws = new ServiceReference1.WebServiceSoapClient();
+        this.GridView1.DataSource = ws.alumno().Tables[0];
+        this.GridView1.DataBind();
+    }
+}
---
+++ pregunta 2/WebSite8/App_Code/WebService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    public int Suma(int a, int b)
+    {
+        return a+b; 
+    }
+
+    public int Resta(int a, int b)
+    {
+        return a - b;
+    }
+
+    [WebMethod]
+    public DataSet alumno()
+    {
+        SqlConnection con = new SqlConnection();
+        SqlDataAdapter ada = new SqlDataAdapter();
+        con.ConnectionString = "server=(local);user=sa;pwd=123;database=academico";
+        ada.SelectCommand = new SqlCommand();
+        ada.SelectCommand.CommandText = "select * from alumno";
+        ada.SelectCommand.CommandType = CommandType.Text;
+        ada.SelectCommand.Connection = con;
+        DataSet ds = new DataSet();
+        ada.Fill(ds);
+        return ds;
+    }
+
+
+}
